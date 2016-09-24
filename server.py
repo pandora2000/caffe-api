655a6b2a7c8a0984594b59b9e56559bb53e2f777
Tetsuri Moriya <tetsuri@abeja.asia>
2016-09-24 19:05:18 +0900

    temporary commit [ci skip]

--- server.py
+++ server.py
@@ -1,117 +1,37 @@
 import os
-from flask import Flask, request, redirect, url_for
-from werkzeug.utils import secure_filename
-from flask import jsonify
-
-from _caffe import lenet
-from caffe import imagenet
-import numpy as np
-from itertools import chain
-
-UPLOAD_FOLDER = os.path.dirname(__file__)
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+from flask import Flask
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+def predict_core(filepath):
+    import caffe
+    import sys
+    import time
 
-@app.route('/lenet', methods=['GET', 'POST'])
-def predict_lenet():
-    if request.method == 'POST':
-        file = request.files['image']
-        if file and allowed_file(file.filename):
-            filename = 'image' + os.path.splitext(file.filename)[1] #secure_filename(file.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
+    def resize_mean(mean):
+        m_min, m_max = mean.min(), mean.max()
+        normal_mean = (mean - m_min) / (m_max - m_min)
+        return caffe.io.resize_image(normal_mean.transpose((1,2,0)), [256, 256]).transpose((2,0,1)) * (m_max - m_min) + m_min
 
-            # Make prediction
-            prediction = net_lenet.predict(filepath)
-            prediction = np.asarray(prediction).flatten()
-            pred_classes = np.argsort(np.asarray(prediction).flatten())[::-1]
-            prediction = np.sort(prediction)[::-1]
+    caffe.set_mode_cpu()
+    mean_filename='/root/work_dir/train/mean.binaryproto'
+    proto_data = open(mean_filename, "rb").read()
+    a = caffe.io.caffe_pb2.BlobProto.FromString(proto_data)
+    mean = caffe.io.blobproto_to_array(a)[0]
+    mean = resize_mean(mean)
+    age_net = caffe.Classifier('/root/agegenderdeeplearning/car_net_definition/deploy.prototxt', '/root/results/caffenet_car_train_iter_34000.caffemodel', mean=mean, channel_swap=(2,1,0), raw_scale=255, image_dims=(256, 256))
+    img = caffe.io.load_image(filepath)
+    scores = age_net.predict([img])
+    return scores[0]
 
-            return jsonify(best=pred_classes[0], \
-                probabilities=prediction.tolist(), predictions=pred_classes.tolist())
-        else:
-            if not allowed_file(file.filename):
-                return jsonify(error="Invalid extension")
+@app.route('/predict', methods=['POST'])
+def predict():
+    file = request.files['image']
+    filepath = os.path.join(os.path.dirname(__file__), 'upload', file.filename)
+    file.save(filepath)
+    data = dict(scores=predict_core(filepath))
+    return flask.jsonify(**data)
 
-    # Show form to upload image
-    return '''
-    <!doctype html>
-    <title>Upload image file</title>
-    <h1>Upload image</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=image>
-         <input type=submit value=Upload>
-    </form>
-    '''
-
-@app.route('/lenet/weights/<layer_name>', methods=['GET'])
-def lenet_weights(layer_name):
-    res = net_lenet.get_weights()
-    return jsonify(weights=res[layer_name].data[:,:,:,:][0].tolist())
-
-@app.route('/lenet/weights/<layer_name>/<int:feature_index>', methods=['GET'])
-def lenet_weights_detail(layer_name, feature_index):
-    blobs = net_lenet.get_weights()
-    return jsonify(weights=blobs[layer_name].data[:,feature_index,:,:][0].tolist())
-
-
-@app.route('/imagenet', methods=['GET', 'POST'])
-def predict_imagenet():
-    if request.method == 'POST':
-        file = request.files['image']
-        if file and allowed_file(file.filename):
-            filename = 'image' + os.path.splitext(file.filename)[1] #secure_filename(file.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-
-            # Make prediction
-            prediction = net_imagenet.predict(filepath)
-            prediction = np.asarray(prediction).flatten()
-            pred_classes = np.argsort(np.asarray(prediction).flatten())[::-1]
-            prediction = np.sort(prediction)[::-1]
-
-            return jsonify(best=pred_classes[0], \
-                probabilities=prediction.tolist(), predictions=pred_classes.tolist())
-        else:
-            if not allowed_file(file.filename):
-                return jsonify(error="Invalid extension")
-
-    # Show form to upload image
-    return '''
-    <!doctype html>
-    <title>Upload image file</title>
-    <h1>Upload image</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=image>
-         <input type=submit value=Upload>
-    </form>
-    '''
-
-if __name__ == '__main__':      
-    ''' We provide access to two trained models:
-        1. Lenet trained with MNISTClassifier for handwritten digit recognition
-        2. Imagenet for object recognition 
-        Both models are loaded before the web server. '''
-
-    # Set the path to your trained models
-    trained_models_path = '/home/allan/Git/caffe/examples'
-
-    net_lenet = lenet.MNISTClassifier(os.path.join(trained_models_path, 'lenet/lenet.prototxt'), \
-                                        os.path.join(trained_models_path, 'lenet/lenet_iter_10000'))
-    net_lenet.caffenet.set_phase_test()
-    net_lenet.caffenet.set_mode_gpu()
-
-                
-    net_imagenet = imagenet.ImageNetClassifier(os.path.join(trained_models_path, 'imagenet/imagenet_deploy.prototxt'), \
-                                        os.path.join(trained_models_path, 'imagenet/caffe_reference_imagenet_model'))
-    net_imagenet.caffenet.set_phase_test()
-    net_imagenet.caffenet.set_mode_cpu()
-
+if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0')
